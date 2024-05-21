9772884ab9f8c60b56ad49df11c13015677dbda6
juhan17 <limjuhan17@gmail.com>
2024-05-21 17:25:18 +0900

    [Feat] uimanager까지

--- UnityProject_1_B_juhan/Assets/Scenes/Scripts/Game/CircleObject.cs
+++ UnityProject_1_B_juhan/Assets/Scenes/Scripts/Game/CircleObject.cs
@@ -11,17 +11,23 @@ public class CircleObject : MonoBehaviour
 
     public int index;
 
+    public float EndTime = 0.0f;
+    public SpriteRenderer spriteRenderer;
+
+    public GameManager gameManager;
+
     void Awake()
     {
         isUsed = false;
         rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.simulated = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+       gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -72,6 +78,32 @@ public class CircleObject : MonoBehaviour
         isDrag = false;
         isUsed = true;
         rigidbody2D.simulated = true;
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "EndLine")
+        {
+            EndTime += Time.deltaTime;
+
+            if(EndTime > 1)
+            {
+                spriteRenderer.color = new Color(0.9f, 0.2f, 0.2f);
+            }
+            if(EndTime>3)
+            {
+                gameManager.EndGame();
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "EndLine")
+        {
+            EndTime = 0.0f;
+            spriteRenderer.color = Color.white;
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
--- UnityProject_1_B_juhan/Assets/Scenes/Scripts/Game/GameManager.cs
+++ UnityProject_1_B_juhan/Assets/Scenes/Scripts/Game/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,18 @@ public class GameManager : MonoBehaviour
     public float TimeCheck;
     public bool isGen;
 
+    public int Point;
+    public int BestScore;
+    public static event Action<int> OnPointChanged;
+    public static event Action<int> OnBestScoreChanged;
+
     // Start is called before the first frame update
     void Start()
     {
+        BestScore = PlayerPrefs.GetInt("BestScore");
         GenObject();
+        OnPointChanged?.Invoke(Point);
+        OnBestScoreChanged?.Invoke(BestScore);
     }
 
     // Update is called once per frame
@@ -23,7 +32,7 @@ public class GameManager : MonoBehaviour
             TimeCheck -= Time.deltaTime;
             if(TimeCheck <=0)
             {
-                int RandNumber = Random.Range(0, 3);
+                int RandNumber = UnityEngine.Random.Range(0, 3);
                 GameObject Temp = Instantiate(CircleObject[RandNumber]);
                 Temp.transform.position = GenTransform.position;
                 isGen = true;
@@ -41,5 +50,20 @@ public class GameManager : MonoBehaviour
         GameObject temp = Instantiate(CircleObject[index]);
         temp.transform.position = position;
         temp.GetComponent<CircleObject>().Used();
+
+        Point += (int)Mathf.Pow(index, 2) * 10;
+        OnPointChanged?.Invoke(Point);
+    }
+
+    public void EndGame()
+    {
+        int BestScore = PlayerPrefs.GetInt("BestScore");
+
+        if(Point > BestScore)
+        {
+            BestScore = Point;
+            PlayerPrefs.SetInt("BestSCore", BestScore);
+            OnBestScoreChanged?.Invoke(BestScore);
+        }
     }
 }
